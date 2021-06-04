470066c91697297b686377f74788d3f6f3ad312b
dalongmaoxiaotushu <sa517009>
2021-06-03 23:56:06 -0700

    20210604

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -59,6 +60,18 @@ public class PaymentController {
         }
 
         return discoveryClient;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeOut(){
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        return serverPort;
+
+
     }
 }
 
